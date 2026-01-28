6901b21f438331d2ad82e5886a370457077c4b4e
fekry34455 <fekry34455@gmail.com>
2026-01-28 18:29:42 +0200

    chore: add dotenv configuration

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
@@ -6,7 +8,7 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
