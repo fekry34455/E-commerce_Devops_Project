0b9886fb88556675a17fb7cb6ffcd215f4910f6b
fekry34455 <fekry34455@gmail.com>
2026-01-28 19:30:35 +0200

    feat: setup controllers and global error handling

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+// Middleware
+app.use(express.json());
 
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
@@ -12,3 +16,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// const dbConnection = require("./config/database");
+// dbConnection();
+const healthRoute = require("./routes/health.routes");
+app.use("/api/health", healthRoute);
+
+const errorHandler = require("./middlewares/error.middleware");
+
+// Error handler (لازم آخر حاجة)
+app.use(errorHandler);
