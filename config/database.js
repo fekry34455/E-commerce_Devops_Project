cd140374750c74b32cf56dc83618f1e92a9afebd
fekry34455 <fekry34455@gmail.com>
2026-01-28 18:31:23 +0200

    feat: add mongodb connection

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const dbConnection = () => {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then((conn) => {
+      console.log(`Database connected: ${conn.connection.host}`);
+    });
+};
+
+module.exports = dbConnection;
